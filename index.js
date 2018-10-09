a8793466cba7d28998c38b16d3f5fdf011c8bd5b
witty-collection-3981 <yisroel96@gmail.com>
2018-10-09 02:08:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,95 @@
-function currentLine(line) {
-  if (!line.length) {
-    return "The line is currently empty."
+var cart = [];
+
+function getCart() {
+ return cart;
+}
+
+function setCart(c) {
+  cart = c;
+  return cart;
+}
+
+function addToCart(item) {
+  var item = generateCartItem(item)
+  getCart().push(item)
+  return `${item.itemName} has been added to your cart.`
+}
+
+function viewCart() {
+  return getCart().length === 0 ? "Your shopping cart is empty." : generateCartDescription()
+}
+
+function total() {
+  var sum = sumUpPrices()
+  return sum
+}
+
+function removeFromCart(itemName) {
+  var itemToRemove = searchCartForItemToRemove(itemName)
+  return itemToRemove ? removeItemFromCart(itemToRemove) : notifyUserThereIsNoItemToRemove()
+}
+
+function placeOrder(cardNumber) {
+  if (arguments[0] == undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    var sumToCharge = total()
+    setCart([])
+    return `Your total cost is $${sumToCharge}, which will be charged to the card ${cardNumber}.`
+  }
+}
+
+// helper functions
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateCartItem(itemName) {
+  return {
+    itemName:itemName,
+    itemPrice:getRandomInt(1, 100)
+  }
+}
+
+function generateCartDescription() {
+  var cartDescription = 'In your cart, you have '
+  if ( getCart().length >= 1 ) {
+    cartDescription += `${getCart()[0].itemName} at $${getCart()[0].itemPrice}`
+  }
+  if ( getCart().length >= 2 ) {
+    var middleCartItemsDescription = ''
+    for (var i=1; i<getCart().length -1; i++) {
+      middleCartItemsDescription += `, ${getCart()[i].itemName} at $${getCart()[i].itemPrice}`
+    }
+    cartDescription += `${middleCartItemsDescription}, and ${getCart()[getCart().length-1].itemName} at $${getCart()[getCart().length-1].itemPrice}`
   }
 
-  const numbersAndNames = []
+  return `${cartDescription}.`
+}
 
-  for (let i = 0, l = line.length; i < l; i++) {
-    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+function searchCartForItemToRemove(itemName) {
+  var searchResult
+  for (var i=0; i<getCart().length; i++) {
+    if (getCart()[i].itemName === itemName) {searchResult = getCart()[i]}
   }
+  return searchResult
+}
 
-  return `The line is currently: ${numbersAndNames.join(', ')}`
-};
-
-function nowServing(line) {
-  if (!line.length) {
-    return "There is nobody waiting to be served!"
+function sumUpPrices() {
+  var sum = 0
+  for (var i=0; i<getCart().length; i++) {
+    sum = sum + getCart()[i].itemPrice
   }
+  return sum
+}
 
-  return `Currently serving ${line.shift()}.`
-};
+function notifyUserThereIsNoItemToRemove() {
+  return 'That item is not in your cart.'
+}
 
-function takeANumber(line, name) {
-  line.push(name)
-
-  return `Welcome, ${name}. You are number ${line.length} in line.`
-};
+function removeItemFromCart(itemToRemove) {
+  var indexOfItemToRemove = cart.indexOf(itemToRemove)
+  //Array.prototype.splice()
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+  getCart().splice(indexOfItemToRemove,1)
+}
